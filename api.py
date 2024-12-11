2bd0513fdcae8fb97dec90eba88eab0a12e1ce40
bgmijod <chalbhay@hotmail.com>
2024-12-11 09:00:33 +0530

    Update api.py

--- api.py
+++ api.py
@@ -116,7 +116,7 @@ def run_flask_app():
         if not ip or not port or not duration:
             return jsonify({'error': 'Missing parameters'}), 400
 
-        command = f"./Spike {ip} {port} {duration} 1024 400"
+        command = f"./Spike {ip} {port} {duration} 256 1000"
         response = execute_command_async(command, int(duration))
         return jsonify(response)
 
